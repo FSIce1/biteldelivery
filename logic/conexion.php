c7660ab24a9fc4c88663684c0876154e44b9295e
juniorjaviersanchez <juniorsc750@gmail.com>
2022-11-08 23:11:03 -0500

    update credenciales

--- logic/conexion.php
+++ logic/conexion.php
@@ -1,12 +1,12 @@
 <?php
 
     $host = "localhost";
-    $user = "root";
-    $clave = "";
-    $bd = "biteldelivery";
+    $user = "kansfjpu_userbitel";
+    $clave = "ctkdaI#p5ih.";
+    $bd = "kansfjpu_bitel";
 
     try{
-        $conectar = mysqli_connect("localhost","root","","biteldelivery");
+        $conectar = mysqli_connect($host,$user,$clave,$bd);
         mysqli_set_charset($conectar, "utf8");
     }catch(Exception $e)
     {
