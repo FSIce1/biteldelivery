d0a7fc62b814e9d6c792fba04ff78e633b7b4f0e
Luis Felipe Siesquen Valdivia <41356321+FSIce1@users.noreply.github.com>
2022-11-12 11:52:16 -0500

    Cambios Cliente II - Bitel Delivery

--- logic/conexion.php
+++ logic/conexion.php
@@ -1,9 +1,16 @@
 <?php
 
+    //* Producción
     $host = "localhost";
     $user = "kansfjpu_userbitel";
     $clave = "ctkdaI#p5ih.";
     $bd = "kansfjpu_bitel";
+
+    //*local
+    $host = "localhost";
+    $user = "root";
+    $clave = "";
+    $bd = "biteldelivery";
 
     try{
         $conectar = mysqli_connect($host,$user,$clave,$bd);
